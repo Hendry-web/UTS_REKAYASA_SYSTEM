a20efad99202123bf325073e787b8b7a1b0e7f8c
wokesdev <48148008+wokesdev@users.noreply.github.com>
2020-10-25 17:49:04 +0700

    Update tambahSiswa.php

--- uts/siswa/tambahSiswa.php
+++ uts/siswa/tambahSiswa.php
@@ -1,36 +1,30 @@
 <?php
- 
- 
- 
+if($_SERVER['REQUEST_METHOD']=='POST'){
+	//Mendapatkan Nilai Variable
+	$paket = $_POST['paket'];
+	$induk = $_POST['induk'];
+	$nama = $_POST['nama'];
+	$jenis = $_POST['jenis'];
+	$tempat = $_POST['tempat'];
+	$tanggal = $_POST['tanggal'];
+	$sekolah = $_POST['sekolah'];
+	$alamat = $_POST['alamat'];
+	$wali = $_POST['wali'];
+	$telp = $_POST['telp'];
 
- 
-	if($_SERVER['REQUEST_METHOD']=='POST'){
-		
-		//Mendapatkan Nilai Variable
-		$paket = $_POST['paket'];
-		$induk = $_POST['induk'];
-		$nama = $_POST['nama'];
-		$jenis = $_POST['jenis'];
-		$tempat = $_POST['tempat'];
-		$tanggal = $_POST['tanggal'];
-		$sekolah = $_POST['sekolah'];
-		$alamat = $_POST['alamat'];
-		$wali = $_POST['wali'];
-		$telp = $_POST['telp'];
+	//Pembuatan Syntax SQL
+	$sql = "INSERT INTO siswa (paket_id,no_induk,nama,jenis_kelamin,tempat_lahir,tanggal_lahir,sekolah_asal_id,alamat,nama_wali,telp) VALUES ('$paket','$induk','$nama','$jenis','$tempat','$tanggal','$sekolah','$alamat','$wali','$telp')";
 
-		//Pembuatan Syntax SQL
-		$sql = "INSERT INTO siswa (paket_id,no_induk,nama,jenis_kelamin,tempat_lahir,	tanggal_lahir,sekolah_asal_id,alamat,nama_wali,telp) VALUES ('$paket','$induk','$nama','$jenis','$tempat','$tanggal','$sekolah','$alamat','$wali','$telp')";
-		
-		//Import File Koneksi database
-		require_once('koneksi.php');
-		
-		//Eksekusi Query database
-		if(mysqli_query($con,$sql)){
-			echo 'Berhasil Menambahkan Pegawai';
-		}else{
-			echo 'Gagal Menambahkan Pegawai';
-		}
-		
-		mysqli_close($con);
+	//Import File Koneksi database
+	require_once('koneksi.php');
+
+	//Eksekusi Query database
+	if(mysqli_query($con,$sql)){
+		echo 'Berhasil Menambahkan Pegawai';
+	}else{
+		echo 'Gagal Menambahkan Pegawai';
 	}
+
+	mysqli_close($con);
+}
 ?>
