39a9f018c65ee494020ab2844c16e4d54773cf78
wokesdev <48148008+wokesdev@users.noreply.github.com>
2020-10-27 13:15:45 +0700

    Update tambahSiswa.php

--- uts/siswa/tambahSiswa.php
+++ uts/siswa/tambahSiswa.php
@@ -14,24 +14,25 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 	$foto = $_FILES['foto']['name'];
 
 	if($foto != ""){
-        $ekstensi_diperbolehkan = array('png','jpg');
-        $x = explode('.', $foto);
-        $ekstensi = strtolower(end($x));
-        $file_tmp = $_FILES['foto']['tmp_name'];   
-        $angka_acak = rand(1,999);
-        $nama_foto_baru = $angka_acak.'-'.$foto;
+		$ekstensi_diperbolehkan = array('png','jpg');
+		$x = explode('.', $foto);
+		$ekstensi = strtolower(end($x));
+		$file_tmp = $_FILES['foto']['tmp_name'];   
+		$angka_acak = rand(1,999);
+		$nama_foto_baru = $angka_acak.'-'.$foto;
 
-        if(in_array($ekstensi, $ekstensi_diperbolehkan) === true){     
-            move_uploaded_file($file_tmp, 'foto/'.$nama_foto_baru);
-            $query = "INSERT INTO siswa (paket_id, no_induk, nama, jenis_kelamin, tempat_lahir, tanggal_lahir, sekolah_asal_id, alamat, nama_wali, telp, foto) VALUES ('$paket', '$induk', '$nama', '$jenis', '$tempat', '$tanggal', '$sekolah', '$alamat', '$wali', '$telp', '$nama_foto_baru')";
-            $result = mysqli_query($con, $query);
+		if(in_array($ekstensi, $ekstensi_diperbolehkan) === true){     
+			move_uploaded_file($file_tmp, 'foto/'.$nama_foto_baru);
+			$query = "INSERT INTO siswa (paket_id, no_induk, nama, jenis_kelamin, tempat_lahir, tanggal_lahir, sekolah_asal_id, alamat, nama_wali, telp, foto) VALUES ('$paket', '$induk', '$nama', '$jenis', '$tempat', '$tanggal', '$sekolah', '$alamat', '$wali', '$telp', '$nama_foto_baru')";
+			$result = mysqli_query($con, $query);
 
-		if($result){
-                	echo 'Berhasil menambahkan siswa.';
-		} 
+			if($result){
+				echo 'Berhasil menambahkan siswa.';
+			} 
 
-		else{
-			echo 'Gagal menambahkan siswa.';
+			else{
+				echo 'Gagal menambahkan siswa.';
+			}
 		}
 	}
 		
